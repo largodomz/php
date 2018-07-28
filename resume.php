54d6ffe2e3e70dd0848fb7dfbc90741c24873fd6
largodomz <38096980+largodomz@users.noreply.github.com>
2018-07-28 14:15:32 +0800

    domz resume

---
+++ resume.php
@@ -0,0 +1,125 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+     <title>Domz Resume</title>
+
+     <style type="text/css">
+
+        * { margin: 0; padding: 0; }
+
+        body { font: 16px Helvetica, Sans-Serif; line-height: 24px; background: url(images/noise.jpg); }
+
+        .clear { clear: both; }
+
+        #page-wrap { width: 800px; margin: 40px auto 60px; }
+
+        #pic { float: right; margin: -30px 0 0 0; }
+
+        h1 { margin: 0 0 16px 0; padding: 0 0 16px 0; font-size: 42px; font-weight: bold; letter-spacing: -2px; border-bottom: 1px solid #999; }
+
+        h2 { font-size: 20px; margin: 0 0 6px 0; position: relative; }
+
+        h2 span { position: absolute; bottom: 0; right: 0; font-style: italic; font-family: Georgia, Serif; font-size: 16px; color: #999; font-weight: normal; }
+
+        p { margin: 0 0 16px 0; }
+
+        a { color: #999; text-decoration: none; border-bottom: 1px dotted #999; }
+
+        a:hover { border-bottom-style: solid; color: black; }
+
+        ul { margin: 0 0 32px 17px; }
+
+        #objective { width: 500px; float: left; }
+
+        #objective p { font-family: Georgia, Serif; font-style: italic; color: #666; }
+
+        dt { font-style: italic; font-weight: bold; font-size: 18px; text-align: right; padding: 0 26px 0 0; width: 150px; float: left; height: 100px; border-right: 1px solid #999;  }
+
+        dd { width: 600px; float: right; }
+
+        dd.clear { float: none; margin: 0; height: 15px; }
+
+     </style>
+
+</head>
+
+<body>
+
+    <div id="page-wrap">
+
+            <img src="images/domz.jpg" alt="Photo of Dominic" id="pic" />
+
+           <div id="contact-info" class="vcard">
+
+                    <!-- Microformats! -->
+
+          <h1 class="fn">DOMINIC LARGO</h1>
+
+          <p><strong> Mobile #</strong>: <span class="tel">0905-395-9863</span><br />
+
+            <strong> Email</strong>: largodomz@gmail.com <br />
+
+            <strong> Address</strong>: 785 Upper Calajo-an Minglanilla, Cebu <br />
+
+            <strong> Course</strong>: Masters in Information Technology <br />
+
+            <strong> Programming</strong>: Java, PhP, Perl, Python, C# </p>
+
+        </div>
+
+  <div id="objective">
+
+        </div>
+
+    <div class="clear"></div>
+
+                <dl>
+
+            <dd class="clear"></dd>
+
+            
+
+            <dt>Goal</dt>
+
+            <dd>
+
+                	<p>One thing I say is, "you have to give respect to get it." Another thing you have learned about me is that I love music. If I didn't have music, I don't know where I would be. That is just how much I love music.I just want to reach all of my desired goals.</p>
+
+            </dd>
+
+              <dd class="clear"></dd>
+
+              <dt>Objective</dt>
+
+            <dd>
+
+                
+
+                <p>As a professional with years of experience under your belt, you are probably writing a resume in order to move to a better working environment, transfer jobs, earn more money, or attain a managerial role. Your Career Objective will need to reflect your wishes, and make a strong argument for why you’d be a strong candidate.</p>
+
+              <dd class="clear"></dd>
+
+      <dt>About Me</dt>
+
+            <dd>I am an outgoing and energetic (ask anybody) young professional, seeking a career that fits my professional skills, personality, and murderous tendencies. My squid-like head is a masterful problem solver and inspires fear in who gaze upon it. I can bring world domination to your organization. </dd>
+
+               <dd class="clear"></dd>
+
+                <dt>Hobbies</dt>
+
+            <dd>World Domination, Deep Sea Diving, Murder Most Foul</dd>
+
+        </dl>
+
+          <div class="clear"></div>
+
+      </div>
+
+</body>
+
+
+
+</html>
