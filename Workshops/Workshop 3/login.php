cfcc5cb0d124f7dda9212c1a712b0a6b2ff00b95
largodomz <38096980+largodomz@users.noreply.github.com>
2018-08-25 13:18:41 +0800

    Update login.php

--- Workshops/Workshop 3/login.php
+++ Workshops/Workshop 3/login.php
@@ -33,7 +33,7 @@ if ( isset($_POST['who']) && isset($_POST['pass']) ) {
 <html>
 <head>
 <?php require_once "bootstrap.php"; ?>
-<title>Ninyoh Login Page</title>
+<title>Largo Login Page</title>
 </head>
 <body>
 <div class="container">
