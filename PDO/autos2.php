1dba6ba776aca26216e173b8b5fccd6e0b61eb34
largodomz <38096980+largodomz@users.noreply.github.com>
2018-09-08 14:19:30 +0800

    Update autos2.php

--- PDO/autos2.php
+++ PDO/autos2.php
@@ -1,4 +1,16 @@
 <?php
+<html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+
 require_once "pdo.php";
 if ( isset($_POST['make']) && isset($_POST['year']) 
      && isset($_POST['mileage'])) {
@@ -37,3 +49,5 @@ foreach ( $rows as $row ) {
 </table>
 
 </body>
+
+     </html>
