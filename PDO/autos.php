0a81c9909c40e1f72cb4de0ee5a981cfe02ef0a4
largodomz <38096980+largodomz@users.noreply.github.com>
2018-09-19 22:03:04 +0800

    Update autos.php

--- PDO/autos.php
+++ PDO/autos.php
@@ -59,8 +59,23 @@ echo $e->getMessage();
 <!DOCTYPE html>
 <html>
 <head>
+    
+ <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+ <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    
+ <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+ <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+ <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    
 <title>Largo Autos Database</title>
-<?php require_once "bootstrap.php"; ?>
+
 </head>
 <style type="text/css">
     .green{
@@ -71,6 +86,10 @@ echo $e->getMessage();
     }
 </style>
 <body>
+    
+ <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+    
 <div class="container">
 <h1>Tracking Autos for <?php echo $_GET['name'];?> </h1>
 <?php
