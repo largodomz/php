2b3383ac88b9fbe52fae63498538d119145e6b3c
largodomz <38096980+largodomz@users.noreply.github.com>
2018-09-19 21:44:09 +0800

    Update index.php

--- PDO/index.php
+++ PDO/index.php
@@ -2,6 +2,19 @@
 <html>
 <head>
 <title>Largo, Dominic - Autos Database</title>
+ <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+ <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+ 
 <?php require_once "bootstrap.php"; ?>
 </head>
 <body>
