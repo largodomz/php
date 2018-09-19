b36fd2fc181f8cd1068ca76e75716d31b326ea6a
largodomz <38096980+largodomz@users.noreply.github.com>
2018-09-19 21:58:47 +0800

    Update login.php

--- PDO/login.php
+++ PDO/login.php
@@ -37,10 +37,26 @@ if ( isset($_POST['who']) && isset($_POST['pass']) ) {
 <!DOCTYPE html>
 <html>
 <head>
-<?php require_once "bootstrap.php"; ?>
+<!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    
+       <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <title>Largo Login Page</title>
 </head>
 <body>
+    
+     <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+    
 <div class="container">
 <h1>Please Log In</h1>
 <?php
